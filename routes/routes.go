1de38ebf7f3bc1e9da5501e737213e2a14712a25
rutvuku <rutvikakarsh69@gmail.com>
2024-05-03 01:52:51 +0530

    first commit

---
+++ routes/routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/Rutvuku/go_restro/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func IncomingRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("/user/signup", controllers.SignUp())
+	incomingRoutes.POST("/user/login", controllers.Login())
+	incomingRoutes.POST("/admin/addproduct", controllers.ProductViewerAdmin())
+	incomingRoutes.GET("/user/productview", controllers.SearchProduct())
+	incomingRoutes.GET("/user/search", controllers.SearchProductByQuery())
+}
