dcfa36d268bd0576c6d4851e5ec58359bed9b460
rutvuku <rutvikakarsh69@gmail.com>
2024-05-13 20:06:53 +0530

    controllers add to cart function added

--- controllers/cart.go
+++ controllers/cart.go
@@ -1,9 +1,58 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"time"
 
-func AddToCart() gin.HandlerFunc {
+	"github.com/Rutvuku/go_restro/database"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
+type Application struct {
+	prodCollection *mongo.Collection
+	userCollection *mongo.Collection
+}
+
+func NewApplication(prodCollection *mongo.Collection, userCollection *mongo.Collection) *Application {
+	return &Application{
+		prodCollection: prodCollection,
+		userCollection: userCollection,
+	}
+}
+
+func (app *Application) AddToCart() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		productQueryId := c.Query("id")
+		if productQueryId == "" {
+			log.Fatal("product id is empty")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("product id is empty"))
+			return
+		}
+		userQueryId := c.Query("userID")
+		if userQueryId == "" {
+			log.Fatal("user id is empty")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("user id is empty"))
+		}
+		productID, err := primitive.ObjectIDFromHex(productQueryId)
+		if err != nil {
+			log.Fatal(err)
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		err = database.AddProductToCart(ctx, app.prodCollection, app.userCollection, productID, userQueryId)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, err)
+
+		}
+		c.IndentedJSON(200, "succesfully added to cart")
+	}
 }
 
 func RemoveItem() gin.HandlerFunc {
